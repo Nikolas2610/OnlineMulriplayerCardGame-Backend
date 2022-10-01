bc6f51022dfb91c986b5bb10a4a1fd4e13972d71
Nikolas2610 <npsillou@gmail.com>
2022-10-01 16:21:11 +0300

    Add CRUD for posts & Fix Bug from MySql reload

---
+++ src/feed/controllers/feed.controller.ts
@@ -0,0 +1,34 @@
+import { Body, Controller, Delete, Get, Param, Post, Put } from '@nestjs/common';
+import { DeleteResult, UpdateResult } from 'typeorm';
+import { FeedPost } from '../models/post.interface';
+import { FeedService } from '../services/feed.service';
+
+@Controller('feed')
+export class FeedController {
+    constructor(
+        private feedService: FeedService
+    ) {}
+
+    @Post()
+    async create(@Body() feedPost: FeedPost): Promise<FeedPost> {
+        return await this.feedService.createPost(feedPost);
+    }
+
+    @Get()
+    findAllPosts(): Promise<FeedPost[]> {
+        return this.feedService.findAllPosts()
+    }
+
+    @Put(':id')
+    updatePost(
+        @Param() id: number,
+        @Body() feedPost: FeedPost
+    ): Promise<UpdateResult> {
+        return this.feedService.updatePost(id, feedPost);
+    }
+
+    @Delete(':id')
+    deletePost(@Param('id') id:number): Promise<DeleteResult> {
+        return this.feedService.deletePost(id);
+    }
+}
---
+++ src/feed/services/feed.service.ts
@@ -0,0 +1,29 @@
+import { Injectable } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { DeleteResult, Repository, UpdateResult } from 'typeorm';
+import { FeedPostEntity } from '../models/post.entity';
+import { FeedPost } from '../models/post.interface';
+
+@Injectable()
+export class FeedService {
+    constructor(
+        @InjectRepository(FeedPostEntity)
+        private readonly feedPostRepository: Repository<FeedPostEntity>
+    ) {}
+
+    async createPost(feedPost: FeedPost): Promise<FeedPost> {
+        return await this.feedPostRepository.save(feedPost);
+    }
+
+    async findAllPosts(): Promise<FeedPost[]> {
+        return await this.feedPostRepository.find();
+    }
+
+    async updatePost(id: number, feedPost: FeedPost): Promise<UpdateResult> {
+        return await this.feedPostRepository.update(id, feedPost);
+    }
+
+    async deletePost(id: number): Promise<DeleteResult> {
+        return await this.feedPostRepository.delete(id);
+    }
+}
