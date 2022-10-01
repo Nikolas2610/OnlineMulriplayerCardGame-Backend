911d38f45d9e74a21147cf9b53e799d424e38b08
Nikolas2610 <npsillou@gmail.com>
2022-10-01 15:12:49 +0300

    Set Mysql Database with DB

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,22 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config'
+import { TypeOrmModule } from '@nestjs/typeorm';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 
 @Module({
-  imports: [],
+  imports: [ConfigModule.forRoot({
+    isGlobal: true
+  }), TypeOrmModule.forRoot({
+    type: 'mysql',
+    host: process.env.MYSQL_HOST,
+    port: parseInt(<string>process.env.MYSQL_PORT),
+    username: process.env.MYSQL_USERNAME,
+    password: process.env.MYSQL_PASSWORD,
+    database: process.env.MYSQL_DATABASE,
+    autoLoadEntities: true,
+    synchronize: true
+  })],
   controllers: [AppController],
   providers: [AppService],
 })
--- src/main.ts
+++ src/main.ts
@@ -3,6 +3,8 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  // start url with api after domain
+  app.setGlobalPrefix('api')
   await app.listen(3000);
 }
 bootstrap();
