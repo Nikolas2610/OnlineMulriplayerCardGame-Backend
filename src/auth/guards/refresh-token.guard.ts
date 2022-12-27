6d729ce3521549f854e6e06579b27e12e061e39d
Nikolas2610 <npsillou@gmail.com>
2022-12-27 14:41:03 +0200

    Add refresh token and access token && Add refresh token guard

--- src/auth/guards/refresh-token.guard.ts
+++ src/auth/guards/refresh-token.guard.ts
@@ -1,20 +1,60 @@
-import { CanActivate, ExecutionContext, Injectable } from '@nestjs/common';
+import { CanActivate, ExecutionContext, HttpException, HttpStatus, Injectable, UnauthorizedException } from '@nestjs/common';
 import { Reflector } from '@nestjs/core';
-import { Observable } from 'rxjs';
+import { JwtService } from '@nestjs/jwt';
+import { InjectRepository } from '@nestjs/typeorm';
+import { UsersEntity } from 'src/entities/db/user.entity';
+import { Repository } from 'typeorm';
+import { User } from '../models/user.interface';
+import * as crypto from 'crypto';
+
 
 @Injectable()
 export class RefreshToken implements CanActivate {
-    constructor(private reflector: Reflector) { }
+    constructor(private reflector: Reflector,
+        private jwtService: JwtService,
+        @InjectRepository(UsersEntity)
+        private readonly usersRepository: Repository<UsersEntity>
+    ) { }
 
-    canActivate(
+    async canActivate(
         context: ExecutionContext,
-    ): boolean | Promise<boolean> | Observable<boolean> {
+    ): Promise<boolean> {
         // Get the user from the request
         const { user } = context.switchToHttp().getRequest();
 
-        console.log(user);
         if (user.refresh_token) {
-            return true
+            return await this.verifyToken(user);
         }
     }
+
+    async verifyToken(user: User) {
+        try {
+            const jwt_rt = await this.jwtService.verifyAsync(
+                user.refresh_token,
+                { secret: process.env.JWT_RT_SECRET }
+            );
+
+            if (Date.now() > jwt_rt.exp * 1000) {
+                throw new HttpException('Forbidden', HttpStatus.FORBIDDEN);
+            }
+
+            const refresh_token = this.hashPassword(jwt_rt.refresh_token)
+
+            const refresh_token_exists = await this.usersRepository.findOne(
+                { where: { refresh_token } }
+            );
+
+            if (refresh_token_exists) {
+                return true
+            }
+        } catch (error) {
+            throw new HttpException('Forbidden', HttpStatus.FORBIDDEN);
+        }
+    }
+
+    hashPassword(token: string) {
+        return crypto.createHash('sha256').update(token).digest('hex');
+    }
 }
+
+
--- src/auth/models/user.interface.ts
+++ src/auth/models/user.interface.ts
@@ -7,6 +7,7 @@ export interface User {
     email?: string;
     password?: string;
     isEmailConfirmed?: boolean;
+    refresh_token: string;
     role?: Role;
     posts?: FeedPost[];
 }
