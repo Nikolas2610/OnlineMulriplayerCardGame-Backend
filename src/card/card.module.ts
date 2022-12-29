eee095442be78576bd1165792259c422e631b5af
Nikolas2610 <npsillou@gmail.com>
2022-12-29 20:38:51 +0200

    Add end points for get user and public cards

--- src/card/card.module.ts
+++ src/card/card.module.ts
@@ -1,10 +1,13 @@
 import { Module } from '@nestjs/common';
 import { EntitiesModule } from 'src/entities/entities.module';
+import { CardController } from './controller/card.controller';
 import { CardService } from './services/card.service';
+import { JwtService } from '@nestjs/jwt';
 
 @Module({
     imports: [EntitiesModule],
-    providers: [CardService],
+    controllers: [CardController],
+    providers: [CardService, JwtService],
     exports: [CardService, CardModule],
 
 })
