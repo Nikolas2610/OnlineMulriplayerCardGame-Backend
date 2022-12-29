3474247d35003276611917fa836debbdef6f26e4
Nikolas2610 <npsillou@gmail.com>
2022-12-29 20:38:51 +0200

    Add end points for get user and public cards

--- src/card/controller/card.controller.ts
+++ src/card/controller/card.controller.ts
@@ -1,8 +1,21 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Get, UseGuards, Request } from '@nestjs/common';
+import { JwtGuard } from 'src/auth/guards/jwt.guard';
+import { RefreshToken } from 'src/auth/guards/refresh-token.guard';
+import { CardsEntity } from 'src/entities/db/card.entity';
+import { CardService } from '../services/card.service';
 
 @Controller('card')
+@UseGuards(JwtGuard, RefreshToken)
 export class CardController {
-    constructor() {
+    constructor(private readonly cardService: CardService) { }
 
+    @Get('public')
+    async getPublicCards(): Promise<CardsEntity[]> {
+        return await this.cardService.getPublicCards();
+    }
+
+    @Get('user')
+    async getUserCards(@Request() req: any): Promise<CardsEntity[]> {
+        return await this.cardService.getUserCards(req.user);
     }
 }
